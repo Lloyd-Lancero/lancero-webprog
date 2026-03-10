12e3165ea97e9854918def4d51d5b46055255ff5
Lloyd-Lancero <lavlancero@gmail.com>
2026-03-10 13:24:33 +0800

    lab_ act 1

--- lancero-client/src/App.jsx
+++ lancero-client/src/App.jsx
@@ -7,29 +7,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header className='App Header'>
+        <h1> Welcome to My React App! </h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Name: Lloyd Lancero <br/>
+          Email: lavlancero@gmail.com <br/>
+          Other Personal Info: <a href="https://github.com/Lloyd-Lancero/lancero-webprog"> GitHub Repository</a>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+    </div>
+  );
 }
 
 export default App
