08609af7b79e1530ea0104e64b26b55e871d8667
Lloyd-Lancero <lavlancero@gmail.com>
2026-06-07 00:43:57 +0800

    Rename NavBar import to Navbar

--- lancero-client/src/layouts/Layout.jsx
+++ lancero-client/src/layouts/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import NavBar from '../components/NavBar';
+import NavBar from '../components/Navbar';
 import Footer from "../components/Footer";
 
 const Layout = () => {
